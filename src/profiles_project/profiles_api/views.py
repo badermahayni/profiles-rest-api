4585f321d4217f25d11519fb3c4f53394b2b9027
Bader Mahayni <bader.mahayni@footlevelers.com>
2017-10-06 13:51:18 -0400

    added get put patch delete functions

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 
 # Create your views here.
 class HelloApiView(APIView):
     """Test API View."""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features."""
@@ -18,3 +22,23 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(sefl, request, pk=None):
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method':'delete'})
